9aa4676f1a09182be90651fe4aafa501d918b236
Lokhin Wu <lokhin.wu@live.com>
2017-11-08 11:53:02 -0500

    Draft.js editor

--- src/index.js
+++ src/index.js
@@ -3,45 +3,8 @@ import ReactDOM from 'react-dom';
 import {Editor, EditorState, RichUtils} from 'draft-js';
 import './index.css';
 import App from './App';
+import MyEditor from './components/MyEditor';
 import registerServiceWorker from './registerServiceWorker';
-
-class MyEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editorState: EditorState.createEmpty()
-    };
-    this.onChange = (editorState) => this.setState({editorState});
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-  }
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  }
-  _onItalicClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
-  }
-  _onUnderlineClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-  }
-  handleKeyCommand(command, editorState) {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this._onBoldClick.bind(this)}><strong>b</strong></button>
-        <button onClick={this._onItalicClick.bind(this)}><em>i</em></button>
-        <button onClick={this._onUnderlineClick.bind(this)}><u>u</u></button>
-        <Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange}/>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(
   <MyEditor/>, document.getElementById('root'));
